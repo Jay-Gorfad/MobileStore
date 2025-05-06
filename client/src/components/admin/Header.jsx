a4f4cbc94c79f6302cd69964b952415fe4e74915
Jay <jgorfad223@rku.ac.in>
2025-05-06 10:02:24 +0530

    chnages in productdetails

--- client/src/components/admin/Header.jsx
+++ client/src/components/admin/Header.jsx
@@ -17,18 +17,7 @@ const Header = ({toggleSidebar}) => {
         </button>
         <div className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
           <div className="input-group">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Search for..."
-              aria-label="Search for..."
-              aria-describedby="btnNavbarSearch"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button className="btn btn-primary" id="btnNavbarSearch" type="submit">
-              <i className="fas fa-search"></i>
-            </button>
+           
           </div>
         </div>
         <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
